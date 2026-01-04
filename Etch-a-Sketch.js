16093213b051a790e942978eefef1c9a729a7b0e
Andrew-McCrow <andrewmccrow@gmail.com>
2026-01-04 09:56:03 +0000

    added grid with styles

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,12 @@
+const grid = document.getElementById('etch-a-sketch');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+    grid.appendChild(row);
+}
